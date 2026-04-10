888aa787623539d693d9b1d89d8fcd4df036cca2
Carlgm-22 <c.galvezmena@gmail.com>
2026-04-10 13:37:48 +0200

    chore: Initial commit

---
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/SportsLeagueBackendApplication.kt
@@ -0,0 +1,11 @@
+package es.uib.tfg.sports_league_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SportsLeagueBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<SportsLeagueBackendApplication>(*args)
+}
---
+++ src/test/kotlin/es/uib/tfg/sports_league_backend/SportsLeagueBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package es.uib.tfg.sports_league_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SportsLeagueBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
