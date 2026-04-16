5442eba3eaea869432fb371d95cc8882824de7db
Carlgm-22 <c.galvezmena@gmail.com>
2026-04-16 09:36:07 +0200

    feat: add security config to avoid CORS errors and parse jwt on request

---
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/common/security/SecurityConfig.kt
@@ -0,0 +1,58 @@
+package es.uib.tfg.sports_league_backend.common.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(private val jwtAuthFilter: JwtAuthenticationFilter) {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .cors { }
+            .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests { auth ->
+                auth.requestMatchers("/api/v1/auth/**").permitAll()
+                auth.requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                auth.requestMatchers(HttpMethod.GET, "/api/v1/leagues/**").permitAll()
+                // ... (tus otras reglas)
+                auth.anyRequest().authenticated()
+            }
+            // NUEVO: Añadimos nuestro filtro antes del filtro por defecto de Spring
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+        return http.build()
+    }
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+
+        config.allowCredentials = true
+        config.allowedOriginPatterns = listOf("*") // En prod lo cambiarás por "https://tuweb.com"
+        config.allowedHeaders = listOf("*")
+        config.allowedMethods = listOf("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+}
