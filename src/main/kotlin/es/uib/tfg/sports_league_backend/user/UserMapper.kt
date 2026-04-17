56d1ce093a44dde581e64a1b28681462547c0b04
Carlgm-22 <c.galvezmena@gmail.com>
2026-04-17 22:59:31 +0200

    feat: implement basic mapping from user create dto to entity

--- src/main/kotlin/es/uib/tfg/sports_league_backend/user/UserMapper.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/user/UserMapper.kt
@@ -1,17 +1,33 @@
 package es.uib.tfg.sports_league_backend.user
 
 import es.uib.tfg.sports_league_backend.user.entities.User
+import es.uib.tfg.sportsapi.dto.UserCreateRequest
 import es.uib.tfg.sportsapi.dto.UserDetails
+import java.net.URI
+import java.time.LocalDateTime
 
 // 1. De Entidad (BD) a DTO (Frontend)
 fun User.toDetailsDTO(): UserDetails {
-    return UserDetails(
-        userId = this.id,
+//    return UserDetails(
+//        userId = this.id,
+//        email = this.email,
+//        fullName = "${this.firstName} ${this.lastName}",
+//        category = this.category,
+//        profileImageUrl = URI(this.profileImageUrl),
+//        licenses = this.licenses,
+//        signature = this.signature,
+//        createdAt = this.createdAt.
+//    )
+    TODO("Not yet implemented")
+}
+
+fun UserCreateRequest.toEntity(): User {
+    return User(
         email = this.email,
-        fullName = this.firstName + this.lastName,
-        category = TODO(),
-        profileImageUrl = TODO(),
-        licenses = TODO(),
-        signature = TODO(),
+        firstName = this.firstName,
+        lastName = this.lastName,
+        passwordHash = this.password, // TODO: hashea la contraseña
+        category = this.category,
+        createdAt = LocalDateTime.now(),
     )
 }
