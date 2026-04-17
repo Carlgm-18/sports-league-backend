5e2786cad7c1989d9fdf69a5353230a43b5f942f
Carlgm-22 <c.galvezmena@gmail.com>
2026-04-17 22:56:54 +0200

    chore: avoid use for testing

--- src/main/kotlin/es/uib/tfg/sports_league_backend/common/security/JwtService.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/common/security/JwtService.kt
@@ -10,40 +10,40 @@ import javax.crypto.SecretKey
 
 @Service
 class JwtService(
-    @Value($$"${jwt.secret}") private val secretKey: String,
-    @Value($$"${jwt.expiration}") private val jwtExpiration: Long
+//    @Value($$"${jwt.secret}") private val secretKey: String,
+//    @Value($$"${jwt.expiration}") private val jwtExpiration: Long
 ) {
-    private fun getSignInKey(): SecretKey {
-        return Keys.hmacShaKeyFor(secretKey.toByteArray())
-    }
-
-    fun generateToken(email: String, userId: Int): String {
-        return Jwts.builder()
-            .claims(mapOf("userId" to userId)) // Metemos el ID del usuario como dato extra
-            .subject(email)
-            .issuedAt(Date(System.currentTimeMillis()))
-            .expiration(Date(System.currentTimeMillis() + jwtExpiration))
-            .signWith(getSignInKey())
-            .compact()
-    }
-
-    fun extractEmail(token: String): String {
-        return extractAllClaims(token).subject
-    }
-
-    private fun extractAllClaims(token: String): Claims {
-        return Jwts.parser()
-            .verifyWith(getSignInKey())
-            .build()
-            .parseSignedClaims(token)
-            .payload
-    }
-
-    fun isTokenValid(token: String): Boolean {
-        return try {
-            extractAllClaims(token).expiration.after(Date())
-        } catch (e: Exception) {
-            false
-        }
-    }
+//    private fun getSignInKey(): SecretKey {
+//        return Keys.hmacShaKeyFor(secretKey.toByteArray())
+//    }
+//
+//    fun generateToken(email: String, userId: Int): String {
+//        return Jwts.builder()
+//            .claims(mapOf("userId" to userId)) // Metemos el ID del usuario como dato extra
+//            .subject(email)
+//            .issuedAt(Date(System.currentTimeMillis()))
+//            .expiration(Date(System.currentTimeMillis() + jwtExpiration))
+//            .signWith(getSignInKey())
+//            .compact()
+//    }
+//
+//    fun extractEmail(token: String): String {
+//        return extractAllClaims(token).subject
+//    }
+//
+//    private fun extractAllClaims(token: String): Claims {
+//        return Jwts.parser()
+//            .verifyWith(getSignInKey())
+//            .build()
+//            .parseSignedClaims(token)
+//            .payload
+//    }
+//
+//    fun isTokenValid(token: String): Boolean {
+//        return try {
+//            extractAllClaims(token).expiration.after(Date())
+//        } catch (e: Exception) {
+//            false
+//        }
+//    }
 }
