cb973366ccbc7a0802ed059211c8835db4dc3898
Carlgm-22 <c.galvezmena@gmail.com>
2026-04-17 22:58:00 +0200

    chore: avoid use for testing

--- src/main/kotlin/es/uib/tfg/sports_league_backend/team/TeamController.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/team/TeamController.kt
@@ -8,36 +8,36 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/api/v1")
 class TeamController(
-    private val teamService: TeamService
+    //private val teamService: TeamService
 ) {
-
-    // Rutas que cuelgan de League
-    @PostMapping("/leagues/{leagueId}/teams")
-    @ResponseStatus(HttpStatus.CREATED)
-    fun requestTeamCreation(
-        @PathVariable leagueId: Int,
-        @Valid @RequestBody request: TeamCreateRequest
-    ) {
-        teamService.requestCreation(leagueId, request)
-    }
-
-    @GetMapping("/leagues/{leagueId}/teams")
-    fun getTeamsByLeague(@PathVariable leagueId: Int): List<TeamDetails> {
-        return teamService.findByLeagueId(leagueId)
-    }
-
-    // Rutas directas del Equipo
-    @GetMapping("/teams/{teamId}")
-    fun getTeamDetails(@PathVariable teamId: Int): TeamDetails { // El DTO unificado usando allOf
-        return teamService.getTeamDetails(teamId)
-    }
-
-    @PostMapping("/teams/{teamId}/join-requests")
-    @ResponseStatus(HttpStatus.CREATED)
-    fun requestToJoinTeam(
-        @PathVariable teamId: Int,
-        @Valid @RequestBody request: TeamJoinRequest
-    ) {
-        teamService.requestToJoin(teamId, request)
-    }
+//
+//    // Rutas que cuelgan de League
+//    @PostMapping("/leagues/{leagueId}/teams")
+//    @ResponseStatus(HttpStatus.CREATED)
+//    fun requestTeamCreation(
+//        @PathVariable leagueId: Int,
+//        @Valid @RequestBody request: TeamCreateRequest
+//    ) {
+//        teamService.requestCreation(leagueId, request)
+//    }
+//
+//    @GetMapping("/leagues/{leagueId}/teams")
+//    fun getTeamsByLeague(@PathVariable leagueId: Int): List<TeamDetails> {
+//        return teamService.findByLeagueId(leagueId)
+//    }
+//
+//    // Rutas directas del Equipo
+//    @GetMapping("/teams/{teamId}")
+//    fun getTeamDetails(@PathVariable teamId: Int): TeamDetails { // El DTO unificado usando allOf
+//        return teamService.getTeamDetails(teamId)
+//    }
+//
+//    @PostMapping("/teams/{teamId}/join-requests")
+//    @ResponseStatus(HttpStatus.CREATED)
+//    fun requestToJoinTeam(
+//        @PathVariable teamId: Int,
+//        @Valid @RequestBody request: TeamJoinRequest
+//    ) {
+//        teamService.requestToJoin(teamId, request)
+//    }
 }
--- src/main/kotlin/es/uib/tfg/sports_league_backend/user/UserService.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/user/UserService.kt
@@ -3,16 +3,13 @@ package es.uib.tfg.sports_league_backend.user
 import es.uib.tfg.sports_league_backend.common.exceptions.UserAlreadyExistsException
 import es.uib.tfg.sports_league_backend.common.security.JwtService
 import es.uib.tfg.sports_league_backend.user.entities.User
-import es.uib.tfg.sportsapi.dto.ImageUrl
 import es.uib.tfg.sportsapi.dto.UserAuthResponse
 import es.uib.tfg.sportsapi.dto.UserCreateRequest
-import es.uib.tfg.sportsapi.dto.UserDetails
 import es.uib.tfg.sportsapi.dto.UserLoginRequest
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.net.URI
 import java.util.UUID
 
 @Service
@@ -20,7 +17,7 @@ class UserService(
     private val userRepository: UserRepository,
     private val passwordEncoder: PasswordEncoder,
     private val jwtService: JwtService,
-    @Value($$"${jwt.expiration}") private val jwtExpirationMs: Long
+    //@Value($$"${jwt.expiration}") private val jwtExpirationMs: Long
 ) {
 
     @Transactional // Asegura que si algo falla, la base de datos hace un rollback y no guarda a medias
@@ -35,7 +32,7 @@ class UserService(
             lastName = request.lastName,
             email = request.email,
             passwordHash = passwordEncoder.encode(request.password)!!,
-            category = request.category.value
+            category = request.category
         )
 
         // 4. Guardar en PostgreSQL
@@ -46,30 +43,23 @@ class UserService(
 
     fun login(request: UserLoginRequest): UserAuthResponse {
 
-        val user = userRepository.findByEmail(request.email) ?: throw RuntimeException("Credenciales inválidas")
-
-        if (!passwordEncoder.matches(request.password, user.passwordHash)) {
-            throw RuntimeException("Credenciales inválidas")
-        }
-
-        val jwtToken = jwtService.generateToken(user.email, user.id)
-
-        val userDetails = UserDetails(
-            userId = user.id,
-            email = user.email,
-            fullName = user.firstName + user.lastName,
-            category = user.category,
-            profileImageUrl = URI("https://example.com"),
-            licenses = listOf(),
-            signature = ImageUrl(URI("https://example.com"))
-        )
-
-        return UserAuthResponse(
-            accessToken = jwtToken,
-            // Generamos un identificador único aleatorio para el Refresh Token por ahora
-            refreshToken = UUID.randomUUID().toString(),
-            // Convertimos los milisegundos a segundos para el front
-            expiresIn = (jwtExpirationMs / 1000).toInt(), tokenType = "Bearer", user = userDetails
-        )
+//        val user = userRepository.findByEmail(request.email) ?: throw RuntimeException("Credenciales inválidas")
+//
+//        if (!passwordEncoder.matches(request.password, user.passwordHash)) {
+//            throw RuntimeException("Credenciales inválidas")
+//        }
+//
+//        val jwtToken = jwtService.generateToken(user.email, user.id)
+//
+//        val userDetails = user.toDetailsDTO()
+//
+//        return UserAuthResponse(
+//            accessToken = jwtToken,
+//            // Generamos un identificador único aleatorio para el Refresh Token por ahora
+//            refreshToken = UUID.randomUUID().toString(),
+//            // Convertimos los milisegundos a segundos para el front
+//            expiresIn = (jwtExpirationMs / 1000).toInt(), tokenType = "Bearer", user = userDetails
+//        )
+        TODO("Not yet implemented")
     }
 }
