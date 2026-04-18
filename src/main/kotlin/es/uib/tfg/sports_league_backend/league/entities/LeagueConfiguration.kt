d7b681fc498eb090984189997f5adaa13f01027e
Carlgm-22 <c.galvezmena@gmail.com>
2026-04-18 17:30:58 +0200

    feat: create league configuration entity

---
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/league/entities/LeagueConfiguration.kt
@@ -0,0 +1,12 @@
+package es.uib.tfg.sports_league_backend.league.entities
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "league_configuration")
+class LeagueConfiguration {
+    @Id
+    var id: Int = 0
+}
