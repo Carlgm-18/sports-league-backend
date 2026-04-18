0e58509b53a0e6548b3026f7b80050812d8ede10
Carlgm-22 <c.galvezmena@gmail.com>
2026-04-18 17:28:56 +0200

    feat: create team entity

---
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/team/entities/Team.kt
@@ -0,0 +1,32 @@
+package es.uib.tfg.sports_league_backend.team.entities
+
+import es.uib.tfg.sports_league_backend.league.entities.League
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "team")
+class Team(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "league_id")
+    var league: League,
+
+    @Column(nullable = false, length = 100)
+    var name: String,
+
+    @Column(nullable = false, length = 10)
+    var initials: String,
+
+    @Column(columnDefinition = "TEXT")
+    var description: String? = null,
+
+    var motto: String? = null,
+    var primaryColor: Int? = null, // Almacenado como entero hexadecimal
+    var secondaryColor: Int? = null,
+    var iconImageUrl: String? = null,
+    var deletedAt: LocalDateTime? = null
+)
