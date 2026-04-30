1e5d0180a9b6b78dab09b993ce722d43db1f3363
Carlgm-22 <c.galvezmena@gmail.com>
2026-04-30 09:42:08 +0200

    feta: complete jwt service

--- src/main/kotlin/es/uib/tfg/sports_league_backend/common/security/JwtService.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/common/security/JwtService.kt
@@ -1,6 +1,5 @@
 package es.uib.tfg.sports_league_backend.common.security
 
-import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
@@ -10,22 +9,34 @@ import javax.crypto.SecretKey
 
 @Service
 class JwtService(
-//    @Value($$"${jwt.secret}") private val secretKey: String,
-//    @Value($$"${jwt.expiration}") private val jwtExpiration: Long
+    @Value($$"${JWT_SECRET}") private val secretKey: String,
+    @Value($$"${JWT_EXPIRATION}") private val jwtExpiration: Long,
+    @Value($$"${JWT_REFRESH_EXPIRATION}") private val jwtRefreshExpiration: Long
 ) {
-//    private fun getSignInKey(): SecretKey {
-//        return Keys.hmacShaKeyFor(secretKey.toByteArray())
-//    }
-//
-//    fun generateToken(email: String, userId: Int): String {
-//        return Jwts.builder()
-//            .claims(mapOf("userId" to userId)) // Metemos el ID del usuario como dato extra
-//            .subject(email)
-//            .issuedAt(Date(System.currentTimeMillis()))
-//            .expiration(Date(System.currentTimeMillis() + jwtExpiration))
-//            .signWith(getSignInKey())
-//            .compact()
-//    }
+    private fun getSignInKey(): SecretKey {
+        return Keys.hmacShaKeyFor(secretKey.toByteArray())
+    }
+
+    fun generateToken(userId: Int, type: TokenType): String {
+        val tokenExpiration = when (type) {
+            TokenType.ACCESS -> jwtExpiration
+            TokenType.REFRESH -> jwtRefreshExpiration
+        }
+
+        val claims = when (type) {
+            TokenType.ACCESS -> mapOf("userId" to userId)
+            TokenType.REFRESH -> mapOf("userId" to userId)
+        }
+
+        return Jwts.builder()
+            .claims(claims)
+            .subject(userId.toString())
+            .issuedAt(Date(System.currentTimeMillis()))
+            .expiration(Date(System.currentTimeMillis() + tokenExpiration))
+            .signWith(getSignInKey())
+            .compact()
+
+    }
 //
 //    fun extractEmail(token: String): String {
 //        return extractAllClaims(token).subject
---
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/common/security/TokenType.kt
@@ -0,0 +1,6 @@
+package es.uib.tfg.sports_league_backend.common.security
+
+enum class TokenType {
+    ACCESS,
+    REFRESH
+}
