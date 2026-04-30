27c84219f5ff04a2cfe9554854f023d3b31b239d
Carlgm-22 <c.galvezmena@gmail.com>
2026-04-30 09:41:05 +0200

    fix: adapt to hexagonal

---
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/user/infrastructure/mapper/UserMapper.kt
@@ -0,0 +1,91 @@
+package es.uib.tfg.sports_league_backend.user.infrastructure.mapper
+
+import es.uib.tfg.sports_league_backend.user.application.LoginSessionInfo
+import es.uib.tfg.sports_league_backend.user.application.UserLoginCommand
+import es.uib.tfg.sports_league_backend.user.application.UserRegisterCommand
+import es.uib.tfg.sports_league_backend.user.domain.SecurePassword
+import es.uib.tfg.sports_league_backend.user.domain.User
+import es.uib.tfg.sportsapi.dto.UserAuthResponse
+import es.uib.tfg.sportsapi.dto.UserCreateRequest
+import es.uib.tfg.sportsapi.dto.UserCreateResponse
+import es.uib.tfg.sportsapi.dto.UserDetails
+import es.uib.tfg.sportsapi.dto.UserLoginRequest
+import es.uib.tfg.sportsapi.dto.UserSummary
+import java.net.URI
+import java.time.LocalDateTime
+
+// users/me endpoint
+fun User.toDetailsDTO() =
+    UserDetails(
+        userId = this.id,
+        email = this.email,
+        fullName = "${this.firstName} ${this.lastName}",
+        category = this.category,
+        profileImageUrl = URI(this.profileImageUrl ?: ""),
+        licenses = this.licenses ?: listOf(),
+        signature = this.signature,
+        createdAt = this.createdAt
+    )
+
+// users/register endpoint
+fun UserCreateRequest.toCommand() =
+    UserRegisterCommand(
+        email = this.email,
+        firstName = this.firstName,
+        lastName = this.lastName,
+        category = this.category,
+        createdAt = LocalDateTime.now(),
+        licenses = this.licenses,
+        password = this.password,
+        confirmPassword = this.confirmPassword,
+        profileImageUrl = "",
+        signature = null
+    )
+
+fun UserRegisterCommand.toEntity(securePassword: SecurePassword) =
+    User(
+        id,
+        firstName,
+        lastName,
+        email,
+        securePassword,
+        category,
+        createdAt,
+        profileImageUrl,
+        licenses,
+        signature,
+    )
+
+fun User.toCreateResponse() =
+    UserCreateResponse(
+        email = this.email,
+        firstName = this.firstName,
+        lastName = this.lastName,
+        licenses = this.licenses,
+        category = this.category,
+    )
+
+fun User.toSummary() =
+    UserSummary(
+        id,
+        "$firstName $lastName",
+        category,
+        URI(profileImageUrl ?: ""),
+    )
+
+// users/login endpoint
+
+fun UserLoginRequest.toCommand() =
+    UserLoginCommand(
+        email,
+        password,
+    )
+
+fun LoginSessionInfo.toLoginResponse() =
+    UserAuthResponse(
+        accessToken,
+        refreshToken,
+        expiresIn.toInt(),
+        tokenType,
+        user.toSummary(),
+    )
