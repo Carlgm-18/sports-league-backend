3d2e0713483341a156a58881d2be712cb62bb3bc
Carlgm-22 <c.galvezmena@gmail.com>
2026-05-06 22:18:48 +0200

    feat: can now retrieve leagues from database

--- src/main/kotlin/es/uib/tfg/sports_league_backend/league/infrastructure/controller/LeagueController.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/league/infrastructure/controller/LeagueController.kt
@@ -1,10 +1,13 @@
 package es.uib.tfg.sports_league_backend.league.infrastructure.controller
 
 import es.uib.tfg.sports_league_backend.league.application.LeagueService
+import es.uib.tfg.sports_league_backend.league.infrastructure.mapper.toDetailsDTO
+import es.uib.tfg.sports_league_backend.league.infrastructure.mapper.toSummaryDTO
 import es.uib.tfg.sportsapi.dto.ConfigurationDetails
 import es.uib.tfg.sportsapi.dto.ConfigurationUpdateRequest
 import es.uib.tfg.sportsapi.dto.LeagueCreateRequest
 import es.uib.tfg.sportsapi.dto.LeagueDetails
+import es.uib.tfg.sportsapi.dto.LeagueSummary
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
@@ -23,19 +26,19 @@ class LeagueController(
 ) {
 
     @GetMapping
-    fun getAllLeagues(): List<LeagueDetails> {
-        return leagueService.findAll()
+    fun getAllLeagues(): List<LeagueSummary> {
+        return leagueService.findAll().map{ it.toSummaryDTO() }
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun createLeague(@Valid @RequestBody request: LeagueCreateRequest): LeagueDetails {
-        return leagueService.createLeague(request)
+        return leagueService.createLeague(request).toDetailsDTO()
     }
 
     @GetMapping("/{leagueId}")
     fun getLeague(@PathVariable leagueId: Int): LeagueDetails {
-        return leagueService.findById(leagueId)
+        return leagueService.findById(leagueId).toDetailsDTO()
     }
 
     @PatchMapping("/{leagueId}/configuration")
