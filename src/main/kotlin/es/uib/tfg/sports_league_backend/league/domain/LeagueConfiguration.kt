a54a1ef5cbf7beb68079d8d114a2d828121469fe
Carlgm-22 <c.galvezmena@gmail.com>
2026-05-06 09:47:22 +0200

    feat: add league configuration entity

--- src/main/kotlin/es/uib/tfg/sports_league_backend/league/domain/LeagueConfiguration.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/league/domain/LeagueConfiguration.kt
@@ -1,12 +1,32 @@
 package es.uib.tfg.sports_league_backend.league.domain
 
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import es.uib.tfg.sports_league_backend.sport.domain.Sport
+import es.uib.tfg.sportsapi.dto.LeagueCategory
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "league_configuration")
-class LeagueConfiguration {
+class LeagueConfiguration (
     @Id
-    var id: Int = 0
-}
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int = 0,
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    var category: LeagueCategory,
+
+    var minTeamFemaleIntegrants: Int? = null,
+
+    @Column(nullable = false)
+    var minTeamMembers: Int,
+
+    @Column(nullable = false)
+    var maxTeamMembers: Int,
+
+    @Column(nullable = false)
+    var roundDuration: Int,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sport_id", nullable = false)
+    var sportName: Sport
+    )
