d4146f101285a2c732952de32116f0876596b5fd
Carlgm-22 <c.galvezmena@gmail.com>
2026-05-12 09:10:15 +0200

    feat: insert data for "enum" tables

---
+++ src/main/resources/db/migration/V99__insert_initial_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO sport (sport_name)
+VALUES ('VOLLEYBALL'), ('FOOTBALL'), ('PADEL')
+ON CONFLICT (sport_name) DO NOTHING;
+
+INSERT INTO participation_role (role_name)
+VALUES ('ADMIN'), ('REFEREE'), ('CAPTAIN'), ('PLAYER')
+ON CONFLICT (role_name) DO NOTHING;
