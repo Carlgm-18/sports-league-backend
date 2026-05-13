b687465dc18ba0e27cf4dedd4a0c2e92eb8b1be6
Carlgm-22 <c.galvezmena@gmail.com>
2026-05-13 18:36:28 +0200

    fix: add change of punctuation rules and system

--- src/main/resources/db/migration/V99__insert_initial_data.sql
+++ src/main/resources/db/migration/V99__insert_initial_data.sql
@@ -5,3 +5,5 @@ ON CONFLICT (sport_name) DO NOTHING;
 INSERT INTO participation_role (role_name)
 VALUES ('ADMIN'), ('REFEREE'), ('CAPTAIN'), ('PLAYER')
 ON CONFLICT (role_name) DO NOTHING;
+
+-- TODO: Add punctuation system and rules inserst
