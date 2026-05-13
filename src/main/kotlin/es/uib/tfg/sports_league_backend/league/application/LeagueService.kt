c9800b024468745dfb97eddc8b9204359cccf8e2
Carlgm-22 <c.galvezmena@gmail.com>
2026-05-13 18:37:33 +0200

    fix: continue the creation league use case

--- src/main/kotlin/es/uib/tfg/sports_league_backend/league/application/LeagueService.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/league/application/LeagueService.kt
@@ -1,25 +1,44 @@
 package es.uib.tfg.sports_league_backend.league.application
 
 import es.uib.tfg.sports_league_backend.league.domain.League
+import es.uib.tfg.sports_league_backend.league.domain.LeagueConfiguration
 import es.uib.tfg.sports_league_backend.league.infrastructure.mapper.toDetailsDTO
+import es.uib.tfg.sports_league_backend.league.infrastructure.repository.LeagueConfigurationRepository
 import es.uib.tfg.sports_league_backend.league.infrastructure.repository.LeagueRepository
+import es.uib.tfg.sports_league_backend.punctuation.infrastructure.repository.PunctuationSystemRepository
 import es.uib.tfg.sportsapi.dto.ConfigurationDetails
 import es.uib.tfg.sportsapi.dto.ConfigurationUpdateRequest
 import es.uib.tfg.sportsapi.dto.LeagueCreateRequest
 import es.uib.tfg.sportsapi.dto.LeagueDetails
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 
 @Service
 class LeagueService(
-    private val leagueRepository: LeagueRepository
+    private val leagueRepository: LeagueRepository,
+    private val leagueConfigurationRepository: LeagueConfigurationRepository,
+    private val punctuationSystemRepository: PunctuationSystemRepository
 ) {
     fun findAll(): List<League> =
         leagueRepository.findAll()
 
-    fun createLeague(request: LeagueCreateRequest): League {
+    @Transactional
+    fun createLeague(league: League): League {
         // TODO: Implement database logic
-        TODO("Not yet implemented")
+        // Save league, save phases, save configuration, save punctuation rules
+
+        val config = league.configuration
+        if(config.id == null) {
+            leagueConfigurationRepository.save(config)
+        }
+
+        val punctuationSystem = league.punctuationSystem
+        if(punctuationSystem.id == null) {
+            punctuationSystemRepository.save(punctuationSystem)
+        }
+
+        return leagueRepository.save(league)
     }
 
     fun findById(leagueId: Int): League {
