f4a8aa4df878956bcc34944393549f6ba1f12fd4
Carlgm-22 <c.galvezmena@gmail.com>
2026-05-13 18:35:33 +0200

    fix: complete mapper for phase creation

--- src/main/kotlin/es/uib/tfg/sports_league_backend/phase/infrastructure/mapper/PhaseMapper.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/phase/infrastructure/mapper/PhaseMapper.kt
@@ -1,41 +1,23 @@
 package es.uib.tfg.sports_league_backend.phase.infrastructure.mapper
 
-import es.uib.tfg.sports_league_backend.league.domain.League
 import es.uib.tfg.sports_league_backend.match.infrastructure.mapper.toSummaryDTO
 import es.uib.tfg.sports_league_backend.phase.domain.ClassificationGroup
 import es.uib.tfg.sports_league_backend.phase.domain.ClassificationPhase
+import es.uib.tfg.sports_league_backend.phase.domain.Phase
 import es.uib.tfg.sports_league_backend.phase.domain.TournamentPhase
 import es.uib.tfg.sports_league_backend.phase.domain.TournamentSlot
 import es.uib.tfg.sports_league_backend.team.infrastructure.mapper.toSummaryDTO
+import es.uib.tfg.sportsapi.dto.ClassificationGroupCreateRequest
 import es.uib.tfg.sportsapi.dto.ClassificationGroupDetails
-import es.uib.tfg.sportsapi.dto.ClassificationPhaseCreateRequest
 import es.uib.tfg.sportsapi.dto.ClassificationPhaseDetails
+import es.uib.tfg.sportsapi.dto.PhaseCreateRequest
 import es.uib.tfg.sportsapi.dto.PhaseType
-import es.uib.tfg.sportsapi.dto.TournamentPhaseCreateRequest
 import es.uib.tfg.sportsapi.dto.TournamentPhaseDetails
 import es.uib.tfg.sportsapi.dto.TournamentSlotDetails
 
-fun ClassificationPhaseCreateRequest.toEntity(league: League): ClassificationPhase =
-    ClassificationPhase(
-        league,
-        name,
-        startDate,
-        endDate,
-        sequenceOrder,
-    )
-
-fun TournamentPhaseCreateRequest.toEntity(league: League): TournamentPhase =
-    TournamentPhase(
-        league,
-        name,
-        startDate,
-        endDate,
-        sequenceOrder,
-    )
-
 fun ClassificationPhase.toDetailsDTO(): ClassificationPhaseDetails =
     ClassificationPhaseDetails(
-        id,
+        id!!,
         name,
         startDate,
         endDate,
@@ -48,13 +30,21 @@ fun ClassificationPhase.toDetailsDTO(): ClassificationPhaseDetails =
 
 fun ClassificationGroup.toDetailsDTO(): ClassificationGroupDetails =
     ClassificationGroupDetails(
+        name,
         topWinners,
-        groupTeams.map { it.team.toSummaryDTO()  }
+        groupTeams.map { it.team.toSummaryDTO() }
+    )
+
+fun ClassificationGroupCreateRequest.toEntity(): ClassificationGroup =
+    ClassificationGroup(
+        name = groupName,
+        topWinners = topWinners,
+        teamsIds = teamIds
     )
 
 fun TournamentPhase.toDetailsDTO(): TournamentPhaseDetails =
     TournamentPhaseDetails(
-        id,
+        id!!,
         name,
         startDate,
         endDate,
@@ -69,3 +59,24 @@ fun TournamentSlot.toDetailsDTO(): TournamentSlotDetails =
         indexOrder,
         match.toSummaryDTO()
     )
+
+fun PhaseCreateRequest.toEntity(): Phase =
+    when (type) {
+        PhaseType.CLASSIFICATION ->
+            ClassificationPhase(
+                name = name,
+                startDate = startDate,
+                endDate = endDate,
+                sequenceOrder = sequenceOrder,
+                groups = groups!!.map { it.toEntity() }
+            )
+
+        PhaseType.TOURNAMENT ->
+            TournamentPhase(
+                name = name,
+                startDate = startDate,
+                endDate = endDate,
+                sequenceOrder = sequenceOrder,
+                stagesNumber = stagesNumber
+            )
+    }
