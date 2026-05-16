834661cdc205926224ac2be68c0877f2b43adc55
Carlgm-22 <c.galvezmena@gmail.com>
2026-05-16 12:41:50 +0200

    refactor: move files

--- src/main/kotlin/es/uib/tfg/sports_league_backend/punctuation/application/PunctuationSystemService.kt
+++
@@ -1,4 +0,0 @@
-package es.uib.tfg.sports_league_backend.punctuation.application
-
-class PunctuationSystemService {
-}
--- src/main/kotlin/es/uib/tfg/sports_league_backend/punctuation/infrastructure/repository/PunctuationSystemRepository.kt
+++
@@ -1,9 +0,0 @@
-package es.uib.tfg.sports_league_backend.punctuation.infrastructure.repository
-
-import es.uib.tfg.sports_league_backend.punctuation.domain.PunctuationSystem
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface PunctuationSystemRepository: JpaRepository<PunctuationSystem, Long> {
-}
