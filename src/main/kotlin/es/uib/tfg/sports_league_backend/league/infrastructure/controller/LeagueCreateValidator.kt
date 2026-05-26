a98437204ff68eb643597b245f90ea9914dcb4e7
Carlgm-22 <c.galvezmena@gmail.com>
2026-05-26 19:59:02 +0200

    fix: change validator logic

--- src/main/kotlin/es/uib/tfg/sports_league_backend/league/infrastructure/controller/LeagueCreateValidator.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/league/infrastructure/controller/LeagueCreateValidator.kt
@@ -3,7 +3,12 @@ package es.uib.tfg.sports_league_backend.league.infrastructure.controller
 import es.uib.tfg.sportsapi.dto.LeagueCreateRequest
 
 fun LeagueCreateRequest.isValidConfiguration(): Boolean =
-    ((customConfiguration == null) xor (configurationId == null))
+    ((customConfiguration != null) xor (configurationId != null))
+
+//private fun LeagueCreateRequest.exlcusiveConfiguration(): Boolean =
+//    (customConfiguration != null &&) xor (configurationId != null)
+//
+//private fun LeagueCreateRequest.minMaxTeamMembersValidation
 
 fun LeagueCreateRequest.isValidPunctuationSystem(): Boolean =
     ((customPunctuationSystem == null) xor (punctuationSystemId == null))
