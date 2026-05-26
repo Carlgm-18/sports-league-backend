f477ed7c4d948edcc0a6c6f087d7ec15f29d519c
Carlgm-22 <c.galvezmena@gmail.com>
2026-05-26 20:02:38 +0200

    feat: add Details mapper

--- src/main/kotlin/es/uib/tfg/sports_league_backend/participant/infrastructure/mapper/ParticipantMapper.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/participant/infrastructure/mapper/ParticipantMapper.kt
@@ -1,8 +1,11 @@
 package es.uib.tfg.sports_league_backend.participant.infrastructure.mapper
 
 import es.uib.tfg.sports_league_backend.participant.domain.Participant
+import es.uib.tfg.sports_league_backend.participant.domain.ParticipationRole
 import es.uib.tfg.sports_league_backend.team.infrastructure.mapper.toSummaryDTO
+import es.uib.tfg.sportsapi.dto.ParticipantDetails
 import es.uib.tfg.sportsapi.dto.ParticipantSummary
+import es.uib.tfg.sportsapi.dto.ParticipationRoleDTO
 
 fun Participant.toSummaryDTO(): ParticipantSummary =
     ParticipantSummary(
@@ -12,3 +15,17 @@ fun Participant.toSummaryDTO(): ParticipantSummary =
         team?.toSummaryDTO(),
         dorsal,
     )
+
+fun Participant.toDetailsDTO(): ParticipantDetails =
+    ParticipantDetails(
+        id!!,
+        user.id!!,
+        league.id!!,
+        roles.map { it.participationRole.toDTO() }.toList(),
+        joinDate,
+        team?.toSummaryDTO(),
+        dorsal
+    )
+
+fun ParticipationRole.toDTO(): ParticipationRoleDTO =
+    ParticipationRoleDTO.valueOf(roleName)
