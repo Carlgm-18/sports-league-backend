382085806b08c84d5ba8a8064bf464f7ae1525c8
Carlgm-18 <c.galvezmena@gmail.com>
2026-06-04 12:46:41 +0200

    fix: implement the create team based on the request use case

--- src/main/kotlin/es/uib/tfg/sports_league_backend/team/application/TeamService.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/team/application/TeamService.kt
@@ -1,7 +1,10 @@
 package es.uib.tfg.sports_league_backend.team.application
 
 import es.uib.tfg.sports_league_backend.core.DomainResult
+import es.uib.tfg.sports_league_backend.league.domain.League
+import es.uib.tfg.sports_league_backend.request.domain.TeamCreateRequest
 import es.uib.tfg.sports_league_backend.team.domain.Team
+import es.uib.tfg.sports_league_backend.team.domain.error.TeamCreateError
 import es.uib.tfg.sports_league_backend.team.domain.error.TeamNotFound
 import es.uib.tfg.sports_league_backend.team.domain.error.TeamRetrieveError
 import es.uib.tfg.sports_league_backend.team.infrastructure.repository.TeamRepository
@@ -17,4 +20,20 @@ class TeamService(val teamRepository: TeamRepository) {
         teamRepository.findByIdOrNull(teamId)
             ?.let { return DomainResult.Success(it) }
             ?: DomainResult.Failure(TeamNotFound)
+
+    fun createTeamWithRequest(request: TeamCreateRequest, league: League): DomainResult<Team, TeamCreateError> {
+        val team = Team(
+            league = league,
+            name = request.name,
+            initials = request.initials,
+            description = request.description,
+            motto = request.motto,
+            primaryColor = request.primaryColor,
+            secondaryColor = request.secondaryColor,
+            iconImageUrl = request.iconImageUrl
+        )
+        val savedTeam = teamRepository.save(team)
+
+        return DomainResult.Success(savedTeam)
+    }
 }
--- src/main/kotlin/es/uib/tfg/sports_league_backend/team/domain/error/TeamError.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/team/domain/error/TeamError.kt
@@ -1,5 +1,6 @@
 package es.uib.tfg.sports_league_backend.team.domain.error
 
 sealed interface TeamRetrieveError
+sealed interface TeamCreateError
 
 object TeamNotFound : TeamRetrieveError
