8c34b6056074104c256a5352d6e55017dbf8cd8d
Carlgm-22 <c.galvezmena@gmail.com>
2026-06-05 20:34:21 +0200

    feat: create round module

--- src/main/kotlin/es/uib/tfg/sports_league_backend/match/domain/Match.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/match/domain/Match.kt
@@ -1,7 +1,7 @@
 package es.uib.tfg.sports_league_backend.match.domain
 
 import es.uib.tfg.sports_league_backend.participant.domain.Participant
-import es.uib.tfg.sports_league_backend.round.entities.Round
+import es.uib.tfg.sports_league_backend.round.domain.Round
 import es.uib.tfg.sports_league_backend.schedule.domain.DateTimeSlot
 import es.uib.tfg.sports_league_backend.team.domain.Team
 import es.uib.tfg.sportsapi.dto.MatchState
--- src/main/kotlin/es/uib/tfg/sports_league_backend/round/domain/Round.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/round/domain/Round.kt
@@ -1,6 +1,8 @@
-package es.uib.tfg.sports_league_backend.round.entities
+package es.uib.tfg.sports_league_backend.round.domain
 
 import es.uib.tfg.sports_league_backend.phase.domain.Phase
+import es.uib.tfg.sports_league_backend.schedule.domain.DateTimeSlot
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
@@ -9,6 +11,7 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 import java.time.LocalDate
 
@@ -24,5 +27,8 @@ class Round(
     var phase: Phase,
 
     @Column(nullable = false)
-    var firstDay: LocalDate
+    var firstDay: LocalDate,
+
+    @OneToMany(cascade = [CascadeType.ALL])
+    var availability: MutableList<DateTimeSlot> = mutableListOf(),
 )
