b7ae2da1e7f981409b45997fe0bb2e04c6abf1cf
Carlgm-18 <c.galvezmena@gmail.com>
2026-06-05 12:51:49 +0200

    fix: increase legibility while fixing the inverted return bug

--- src/main/kotlin/es/uib/tfg/sports_league_backend/sport/application/SportService.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/sport/application/SportService.kt
@@ -4,6 +4,7 @@ import es.uib.tfg.sports_league_backend.core.DomainResult
 import es.uib.tfg.sports_league_backend.sport.domain.Sport
 import es.uib.tfg.sports_league_backend.sport.domain.errors.SportRetrieveError
 import es.uib.tfg.sports_league_backend.sport.infrastructure.repository.SportRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,13 +14,8 @@ class SportService(
     fun getAllSports(): List<Sport> =
         sportRepository.findAll()
 
-    fun getSportById(sportId: Long): DomainResult<Sport, SportRetrieveError> {
-        val sport = sportRepository.findById(sportId)
-        return if(sport.isPresent) {
-            DomainResult.Failure(SportRetrieveError.SportNotFound)
-        }else {
-            DomainResult.Success(sport.get())
-        }
-
-    }
+    fun getSportById(sportId: Long): DomainResult<Sport, SportRetrieveError> =
+        sportRepository.findByIdOrNull(sportId)
+            ?.let { DomainResult.Success(it) }
+            ?: DomainResult.Failure(SportRetrieveError.SportNotFound)
 }
