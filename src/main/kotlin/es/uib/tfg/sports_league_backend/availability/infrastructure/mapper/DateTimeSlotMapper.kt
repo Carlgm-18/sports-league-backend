f64bbd38f7703c0923f80d1e3695fcd1f67a63c1
Carlgm-22 <c.galvezmena@gmail.com>
2026-06-10 11:39:02 +0200

    chore: change module name

--- src/main/kotlin/es/uib/tfg/sports_league_backend/availability/infrastructure/mapper/DateTimeSlotMapper.kt
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/availability/infrastructure/mapper/DateTimeSlotMapper.kt
@@ -1,6 +1,6 @@
-package es.uib.tfg.sports_league_backend.schedule.infrastructure.mapper
+package es.uib.tfg.sports_league_backend.availability.infrastructure.mapper
 
-import es.uib.tfg.sports_league_backend.schedule.domain.DateTimeSlot
+import es.uib.tfg.sports_league_backend.availability.domain.DateTimeSlot
 import es.uib.tfg.sportsapi.dto.DateTimeSlotDetails
 
 fun DateTimeSlot.toDetailsDTO(): DateTimeSlotDetails =
