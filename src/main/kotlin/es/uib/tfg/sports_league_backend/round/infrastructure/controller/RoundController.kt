c6e92b9a076c98418076e9e307eb3f1ea8c0c1ad
Carlgm-22 <c.galvezmena@gmail.com>
2026-06-10 22:47:10 +0200

    feat: create round controller

---
+++ src/main/kotlin/es/uib/tfg/sports_league_backend/round/infrastructure/controller/RoundController.kt
@@ -0,0 +1,9 @@
+package es.uib.tfg.sports_league_backend.round.infrastructure.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/rounds")
+class RoundController {
+}
