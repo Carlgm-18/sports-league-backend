6fa82254057064be1f970eb538091fd3492aad36
Carlgm-22 <c.galvezmena@gmail.com>
2026-06-19 13:06:08 +0200

    feat: add the controller, service, and repository files within the directories

--- create_module.ps1
+++ create_module.ps1
@@ -4,13 +4,14 @@ param (
 )
 
 $BasePath = "src/main/kotlin/es/uib/tfg/sports_league_backend/$ModuleName"
+$FilesName =  (Get-Culture).TextInfo.ToTitleCase($ModuleName)
 
 $Folders = @(
     # (Domain)
     "$BasePath/domain/errors",
 
     # (Application)
-    "$BasePath/application/",
+    "$BasePath/application",
 
     # (Infrastructure)
     "$BasePath/infrastructure/controller",
@@ -18,11 +19,34 @@ $Folders = @(
     "$BasePath/infrastructure/repository"
 )
 
+$Files = @(
+    # (Domain)
+    "$BasePath/domain/${FilesName}.kt",
+    "$BasePath/domain/errors/${FilesName}Error.kt",
+
+    # (Application)
+    "$BasePath/application/${FilesName}Service.kt",
+
+    # (Infrastructure)
+    "$BasePath/infrastructure/controller/${FilesName}Controller.kt",
+    "$BasePath/infrastructure/mapper/${FilesName}Mapper.kt",
+    "$BasePath/infrastructure/repository/${FilesName}Repository.kt"
+)
+
 foreach ($Folder in $Folders) {
     if (-not (Test-Path $Folder)) {
         New-Item -ItemType Directory -Path $Folder -Force | Out-Null
-        New-Item -ItemType File -Path "$Folder/.gitkeep" -Force | Out-Null
     }
 }
 
-Write-Host "¡Module creted at $BasePath!" -ForegroundColor Green
+foreach ($File in $Files) {
+    if (-not (Test-Path $File)) {
+        $FileDir = Split-Path $File -Parent
+        $RelativeDir = $FileDir -replace "src\\main\\kotlin\\", ""
+        $PackageName = $RelativeDir -replace "[/\\]", "."
+
+        New-Item -ItemType File -Path $File -Value "package $PackageName`n`n" | Out-Null
+    }
+}
+
+Write-Host "Module created at $BasePath!" -ForegroundColor Green
